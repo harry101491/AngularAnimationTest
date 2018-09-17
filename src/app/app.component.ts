406efc33eef7e29b1812d78c1cdb4ae168bad1b8
Harshit Pareek <hp1014@nyu.edu>
2018-09-16 17:40:39 -0700

    feat(adding the animation in angular)

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,23 +3,52 @@ import {
   OnInit
 } from '@angular/core';
 
-import { HeroService, Hero } from './service/hero.service';
+import {
+  trigger,
+  state,
+  style,
+  animate,
+  transition,
+  keyframes
+} from "@angular/animations";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  animations: [
+    trigger("divState", [
+      state("normal", style({
+        "background-color": "red",
+        transform: "translateX(0)"
+      })),
+      state("highlight", style({
+        "background-color": "blue",
+        transform: "translateX(100%)"
+      })),
+      transition("normal => highlight", animate(300)),
+      transition("highlight => normal", animate(800))
+    ])
+  ]
 })
 export class AppComponent implements OnInit {
 
-  heros: Hero[];
+  state = "normal";
+  list = ['Milk', 'Sugar', 'Bread'];
 
-  constructor(
-    private _heroService: HeroService
-  ) {
-    this.heros = this._heroService.heroes;
+  onAdd(item) {
+    this.list.push(item);
+  }
+
+  onDelete(item) {
+    this.list.splice(this.list.indexOf(item), 1);
+  }
+
+  onAnimate() {
+    this.state = this.state === "normal" ? "highlight" : "normal";
   }
 
   ngOnInit() {
+
   }
 }
